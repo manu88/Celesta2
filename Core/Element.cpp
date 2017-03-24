84ddc6476b9698ca62ecd5e09151bf6f3a67fdbe
manu88 <manuel.deneu@gmail.com>
2017-03-24 14:33:49 +0100

    repo reorganization

---
+++ Core/Element.cpp
@@ -0,0 +1,9 @@
+//
+//  Element.cpp
+//  Celesta
+//
+//  Created by Manuel Deneu on 24/03/2017.
+//  Copyright © 2017 Unlimited Development. All rights reserved.
+//
+
+#include "Element.hpp"
---
+++ Core/Element.hpp
@@ -0,0 +1,14 @@
+//
+//  Element.hpp
+//  Celesta
+//
+//  Created by Manuel Deneu on 24/03/2017.
+//  Copyright © 2017 Unlimited Development. All rights reserved.
+//
+
+#ifndef Element_hpp
+#define Element_hpp
+
+#include <stdio.h>
+
+#endif /* Element_hpp */
