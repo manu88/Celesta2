f90039b2f2939f01ec430a54ae802846a155a620
manu88 <manuel.deneu@gmail.com>
2017-03-24 23:07:17 +0100

    Test update

--- CoreTest/GXTestMain.cpp
+++ CoreTest/GXTestMain.cpp
@@ -85,60 +85,71 @@ static void inputCallback( GBRunLoopSource* source , GBRunLoopSourceNotification
 
 int main(int argc, const char * argv[])
 {
-    
-    if(disp.init())
     {
-        std::cout << "Display init OK " << std::endl;
+        if(disp.init())
+        {
+            std::cout << "Display init OK " << std::endl;
+        }
+        else
+        {
+            std::cout << "Display init Error " << std::endl;
+        }
+        
+        
+        GXElement mainElement;
+        mainElement.setBackgroundColor(makeColor(255,0,0));
+
+        const GXRect b = makeRect(0, 0, 1920, 1080);
+        mainElement.setBounds( b );
+        assert(mainElement.getBounds() == b);
+        
+        
+        win1.setBounds(  makeRect(100, 50, 200, 150) );
+        win1.setZPos(0);
+        win1.setBackgroundColor(makeColor(0,255,0));
+
+        
+        child2.setBounds(  makeRect(0, 0, 170, 100) );
+        child2.setZPos(1);
+        child2.setBackgroundColor(makeColor(0,0,255));
+
+        mainElement.addChild(&win1);
+        win1.addChild(&child2);
+        
+        GXRenderer renderer;
+        renderer.setDisplayedElement(&mainElement);
+        renderer.setBounds(  makeRect(0, 0, 1920, 1080) );
+        
+        
+        disp.setRenderer(&renderer);
+
+        
+        if( !disp.update() )
+        {
+            std::cout << "Error update Display " << std::endl;
+        }
+        
+        RunLoop runLoop;
+        
+        
+        GBFDSource* input =  GBFDSourceInitWithFD(STDIN_FILENO, inputCallback);
+        runLoop.addSource(input);
+        
+        
+        Timer timer;
+        timer.setInterval(10);
+        timer.setCallback([&](Timer &timer)
+        {
+            win1.moveOf(5, 5);
+            disp.update();
+        });
+        runLoop.addSource(timer);
+        
+        runLoop.run();
+        
+
+        GBRelease(input);
     }
-    else
-    {
-        std::cout << "Display init Error " << std::endl;
-    }
-    
-    
-    GXElement mainElement;
-    mainElement.setBackgroundColor(makeColor(255,0,0));
-
-    const GXRect b = makeRect(0, 0, 1920, 1080);
-    mainElement.setBounds( b );
-    assert(mainElement.getBounds() == b);
-    
-    
-    win1.setBounds(  makeRect(100, 50, 200, 150) );
-    win1.setZPos(0);
-    win1.setBackgroundColor(makeColor(0,255,0));
-
-    
-    child2.setBounds(  makeRect(0, 0, 170, 100) );
-    child2.setZPos(1);
-    child2.setBackgroundColor(makeColor(0,0,255));
-
-    mainElement.addChild(&win1);
-    win1.addChild(&child2);
-    
-    GXRenderer renderer;
-    renderer.setDisplayedElement(&mainElement);
-    renderer.setBounds(  makeRect(0, 0, 1920, 1080) );
-    
-    
-    disp.setRenderer(&renderer);
-
-    
-    if( !disp.update() )
-    {
-        std::cout << "Error update Display " << std::endl;
-    }
-    
-
-    GBRunLoop* runLoop = GBRunLoopInit();
-    
-    GBFDSource* input =  GBFDSourceInitWithFD(STDIN_FILENO, inputCallback);
-    GBRunLoopAddSource(runLoop, input);
-    GBRunLoopRun(runLoop);
-    
-    GBRelease(runLoop);
-    GBRelease(input);
-    
     Introspection(true);
 
     
