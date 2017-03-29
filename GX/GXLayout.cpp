58e7e674cffa4397e28a4e94129a9fea27f3a5b0
manu88 <manuel.deneu@gmail.com>
2017-03-29 18:27:41 +0200

    Added GXLayout class

---
+++ GX/GXLayout.cpp
@@ -0,0 +1,20 @@
+//
+//  GXLayout.cpp
+//  Celesta
+//
+//  Created by Manuel Deneu on 29/03/2017.
+//  Copyright © 2017 Unlimited Development. All rights reserved.
+//
+
+#include "GXLayout.hpp"
+
+
+GXLayout::GXLayout():
+CLElement("GXLayout")
+{
+    
+}
+GXLayout::~GXLayout()
+{
+    
+}
---
+++ GX/GXLayout.hpp
@@ -0,0 +1,22 @@
+//
+//  GXLayout.hpp
+//  Celesta
+//
+//  Created by Manuel Deneu on 29/03/2017.
+//  Copyright © 2017 Unlimited Development. All rights reserved.
+//
+
+#ifndef GXLayout_hpp
+#define GXLayout_hpp
+
+#include "GXElement.hpp"
+
+class GXLayout : public GXElement
+{
+public:
+    GXLayout();
+    ~GXLayout();
+private:
+};
+
+#endif /* GXLayout_hpp */
